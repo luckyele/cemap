fbad00635e2998a753b02b4383d786c33a44cc9c
luckyele <541913141@qq.com>
2019-02-16 18:48:29 +0800

    add

--- readJson.js
+++ readJson.js
@@ -1,5 +1,5 @@
 
-var points = [];
+
 
 function readJson() {
     var requestURL = "http://192.168.31.116:8080/a.json";
@@ -8,25 +8,42 @@ function readJson() {
     request.responseType = "application/json";
     request.send();
      
-    //处理来自服务器的数据
+    //处理服务器数据
     request.onload = function() {
         var gpsText = request.response;
         var facilities = JSON.parse(gpsText);
         console.log(facilities)
-        // let superHeroes = request.response;
         var dispArea = document.querySelector("#disp");
         
+        map.clearOverlays();
+
         for (var i = 1; i<facilities.length; i++) {
             var p = document.createElement('p');
             
-            var name = facilities[i][0];
-            var gps_x = facilities[i][7].split(',')[0];
-            var gps_y = facilities[i][7].split(',')[1];
+            var name        = facilities[i][0];
+            var id          = facilities[i][1];
+            var hierarchy   = facilities[i][2];
+            var area        = facilities[i][3];
+            var telephon    = facilities[i][4];
+            var address     = facilities[i][5];
+            var level       = facilities[i][6];
+            var gps_x       = facilities[i][7].split(',')[0];
+            var gps_y       = facilities[i][7].split(',')[1];
             
-            p.textContent = facilities[i];
+    
+            var city = document.getElementById("districtName");
+        
+            if (area == city.value) {
+                drawMarker(gps_x, gps_y, name);            
+                p.textContent   = name + " " + gps_x +"  "+ gps_y;
 
-            drawMarker(gps_x, gps_y, name);
-            
+                var circle = new BMap.Circle(new BMap.Point(gps_x, gps_y), 3000);
+                console.log(circle);
+                circle.setFillColor("#A6CBA1"); //填充颜色
+                circle.setStrokeColor("#A6CBA1"); //边线颜色
+                map.addOverlay(circle);            //增加圆
+            }
+                   
             dispArea.appendChild(p);
         }        
     };
@@ -36,16 +53,10 @@ function drawMarker(x,y,name) {
     var point = new BMap.Point(x, y);
     var marker = new BMap.Marker(point);
     marker.setTitle(name);
-    
-    label = new BMap.Label(data.id, {offset: new BMap.Size(20, 0)}); //创建marker点的标记,这里注意下,因为百度地图可以对label样式做编辑,所以我这里吧重要的id放在了label(然后再隐藏)   
-　　　　label.setStyle({display:"none"});//对label 样式隐藏    
-　　　　marker.setLabel(label);  //把label设置到maker上  
-　　　　　　　　　　
-　　　　marker.addEventListener("click", 
-        function (e) {      //这里添加maker的监听点击事件,触发自定义div("#info-panel)的展示    　　　　　　　　　　　　　　　　　　　　　　     
-    　　　　　　　　$("#info-panel").toggle(300);//div展开,关闭        
-　　　　　　　　　　　　me.tag=e.target.getLabel().content; //点击maker点后  获取label里面的内容       
-　　　　　　　　　　　　$("#id").html(e.target.getLabel().content); //这里就可以获取到marker的id       
-　　　　
+    marker.addEventListener("click", function() {
+        alert(name);
+    });
+
     map.addOverlay(marker);    
+    //console.log(point);
 }
